0143ecca326e7dda4e63a245f716c8946059ca2b
skryptx <sinnisingla@gmail.com>
2025-02-17 19:22:51 +0530

    feat: day 2 split the bill

--- Day 2/split_the_bill.py
+++ Day 2/split_the_bill.py
@@ -4,3 +4,4 @@ people_count = float(input("How many people to split the bill? "))
 
 result = round((total_bill+tip*total_bill/100)/people_count, 2)
 print("Each person should pay: " + str(result))
+
