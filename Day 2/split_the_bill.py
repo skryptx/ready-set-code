30b724253e0a2c6b6d884e9c162d609ef83bedcf
skryptx <67511523+skryptx@users.noreply.github.com>
2025-02-17 21:17:41 +0530

    feat: day 2

--- Day 2/split_the_bill.py
+++ Day 2/split_the_bill.py
@@ -1,7 +1,7 @@
 total_bill = float(input("What was the total bill? $"))
-tip = float(input("How much tip would you like to give? 10, 12, or 15? $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
 people_count = float(input("How many people to split the bill? "))
 
 result = round((total_bill+tip*total_bill/100)/people_count, 2)
-print("Each person should pay: " + str(result))
+print(("Each person should pay: " + str(result))[0])
 
---
+++ main.py
@@ -0,0 +1,4 @@
+print(type(122))
+print(type(122.121))
+print("ookdosw" == None)
+print(type(False))
