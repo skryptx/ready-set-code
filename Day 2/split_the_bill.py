b13a19d30a9e3aee48be6c981f46b30f5cadbfe8
skryptx <67511523+skryptx@users.noreply.github.com>
2025-02-18 00:44:13 +0530

    feat: day 2

--- Day 2/split_the_bill.py
+++ Day 2/split_the_bill.py
@@ -3,5 +3,5 @@ tip = float(input("How much tip would you like to give? 10, 12, or 15? "))
 people_count = float(input("How many people to split the bill? "))
 
 result = round((total_bill+tip*total_bill/100)/people_count, 2)
-print(("Each person should pay: " + str(result))[0])
+print(f"Each person should pay: ${result}")
 
