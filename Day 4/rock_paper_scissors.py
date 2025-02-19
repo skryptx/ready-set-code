2dc6a80a6a2d6d3626db9a2b90f52be91d3aa8e6
skryptx <67511523+skryptx@users.noreply.github.com>
2025-02-20 01:03:48 +0530

    feat: day 4 - rock paper scissors

---
+++ Day 4/rock_paper_scissors.py
@@ -0,0 +1,61 @@
+from random import randint
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+sign_mapping = {
+    0: rock,
+    1: paper,
+    2: scissors
+}
+
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for " +
+                    "Scissors.\n"))
+print(sign_mapping[user_input])
+
+print("Computer chose:")
+computer_input = randint(0,2)
+print(sign_mapping[computer_input])
+
+if user_input == computer_input:
+    print("Draw!")
+else:
+    if user_input == 0:
+        if computer_input == 1:
+            print("You Lose!")
+        else:
+            print("You Win!")
+    if user_input == 1:
+        if computer_input == 2:
+            print("You Lose!")
+        else:
+            print("You Win!")
+    if user_input == 2:
+        if computer_input == 0:
+            print("You Lose!")
+        else:
+            print("You Win!")
