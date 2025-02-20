c01b43688bebacb00f1fca23c097be5ca487021a
skryptx <67511523+skryptx@users.noreply.github.com>
2025-02-21 00:34:44 +0530

    feat: day 6 - reeborg hurdles

---
+++ Day 6/reeborg_hurdles.py
@@ -0,0 +1,19 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def cycle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# for i in range(6):
+#     cycle()
