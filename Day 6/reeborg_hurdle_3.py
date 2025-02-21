fd1a2ae0ce7eb01ba03f1c87be1958f8a1104db3
skryptx <67511523+skryptx@users.noreply.github.com>
2025-02-22 00:34:24 +0530

    feat: day 6 - reeborg hurdles 3

---
+++ Day 6/reeborg_hurdle_3.py
@@ -0,0 +1,24 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def cycle():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     while not wall_in_front():
+#         if at_goal():
+#             break
+#         move()
+#     if at_goal():
+#         break
+#     cycle()
