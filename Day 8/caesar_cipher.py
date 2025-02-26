1a37736176151e5b843703ae4b9991770ceb4c22
skryptx <67511523+skryptx@users.noreply.github.com>
2025-02-27 00:48:54 +0530

    feat(day8): caesar cipher decode

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -1,8 +1,19 @@
 def encode(msg, shift):
     encode_list = []
     for char in msg:
-        msg = ((ord(char)- 96) + shift) % 26
-        encode_list.append(chr(msg+96))
+        offset = ((ord(char)- 96) + shift) % 26
+        encode_list.append(chr(offset+96))
     return ''.join(encode_list)
 
-print(encode("piedpiper", 5))
+def decode(msg, shift):
+    encode_list = []
+    for char in msg:
+        offset = ((ord(char)- 96) - shift)%26
+        if offset > 0:
+            encode_list.append(chr(offset + 96))
+        else:
+            encode_list.append(chr(122 - offset))
+    return ''.join(encode_list)
+
+print(encode("piedpiper", 115))
+print(decode("atpoatapc", 115))
