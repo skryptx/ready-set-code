c7255255a646eb89e2179f2fd24595d928d32565
skryptx <67511523+skryptx@users.noreply.github.com>
2025-02-28 00:51:06 +0530

    feat(day8): caesar

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -1,13 +1,19 @@
 def encode(msg, shift) -> str:
     encode_list = []
     for char in msg:
-        offset = ((ord(char)- 96) + shift) % 26
-        encode_list.append(chr(offset+96))
+        if ord(char) not in range(97, 123):
+            encode_list.append(char)
+            continue
+        offset = ((ord(char)- 97) + shift) % 26
+        encode_list.append(chr(offset+97))
     return ''.join(encode_list)
 
 def decode(msg, shift) -> str:
     encode_list = []
     for char in msg:
+        if ord(char) not in range(97, 123):
+            encode_list.append(char)
+            continue
         offset = ((ord(char)- 96) - shift)%26
         if offset > 0:
             encode_list.append(chr(offset + 96))
@@ -15,5 +21,5 @@ def decode(msg, shift) -> str:
             encode_list.append(chr(122 - offset))
     return ''.join(encode_list)
 
-print(encode("piedpiper", 115))
-print(decode("atpoatapc", 115))
+print(encode("hello world!", 9))
+print(decode("qnuux fxaum!", 9))
