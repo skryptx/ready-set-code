3f17ded411ef900d9c88ae2e050da6b547052f42
Sinni Singla <sinnisingla@outlook.com>
2025-03-02 01:06:13 +0530

    feat(Day 9): grading program

---
+++ grading_program.py
@@ -0,0 +1,23 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+student_grades = {}
+
+for student in student_scores:
+    grade = ""
+    if student_scores[student] > 90:
+        grade = "Outstanding"
+    elif student_scores[student] > 80:
+        grade = "Exceeds Expectations"
+    elif student_scores[student] > 70:
+        grade = "Acceptable"
+    else:
+        grade = "Fail"
+
+    student_grades[student] = grade
+
+print(student_grades)
