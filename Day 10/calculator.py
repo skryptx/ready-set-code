4e040355e6d01d340c814e65e311034f7a181eb1
skryptx <67511523+skryptx@users.noreply.github.com>
2025-03-04 00:50:41 +0530

    feat(Day 10): calculator

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -1,6 +1,5 @@
 from utilities.clear_console import clear
 
-
 def apply_operator(f_num: int, s_num: int, operator: str) -> int | float | None:
     if operator == "+":
         return f_num + s_num
