ecf0df405820f5eeb4af0b8a871a57496a25985b
skryptx <67511523+skryptx@users.noreply.github.com>
2025-03-19 13:35:41 +0530

    feat(day 12): guess the number

--- Day_12/guessing_game.py
+++ Day_12/guessing_game.py
@@ -1,8 +1,13 @@
+"""
+This program is a guessing game with limited chances
+
+Returns:
+    _type_: None
+"""
+
 from enum import Enum
 import random
 from art import art
-
-guessCount = 7
 
 
 class Difficulty(Enum):
@@ -22,17 +27,17 @@ def evaluate_guess(actual_num: int, guessed_num: int) -> bool:
     return True
 
 
-def play_guess_the_number(num: int, guess_count: int) -> None:
+def play_guess_the_number(num: int, guesses: int) -> None:
     number_found: bool = False
 
-    while guess_count > 0 and not number_found:
-        print(f"You have {guess_count} attempts remaining to guess the number.")
+    while guesses > 0 and not number_found:
+        print(f"You have {guesses} attempts remaining to guess the number.")
         guess = int(input("Make a guess: "))
         number_found = evaluate_guess(num, guess)
-        guess_count -= 1
+        guesses -= 1
         if number_found:
             return
-        if guess_count > 0 and not number_found:
+        if guesses > 0 and not number_found:
             print("Guess again.")
 
     print("You Lost!")
@@ -40,7 +45,7 @@ def play_guess_the_number(num: int, guess_count: int) -> None:
 
 print(art)
 print("I'm thinking of a number between 1 and 100")
-user_diff_input = input("Choose a difficulty. Type 'easy', 'medium' or 'hard': ")
+user_diff_input = input("Choose a difficulty. Type 'easy','medium' or 'hard':")
 guess_count = int(Difficulty[user_diff_input.upper()].value)
 
 play_guess_the_number(random.randint(1, 100), guess_count)
