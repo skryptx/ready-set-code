717d379b536007ce26f05c69736fdc3e08848b3c
skryptx <67511523+skryptx@users.noreply.github.com>
2025-03-28 12:49:09 +0530

    feat(day 12): guess the number

--- py/01_angela_yu/Day_4/russian_roulette.py
+++ py/01_angela_yu/Day_4/russian_roulette.py
@@ -3,3 +3,10 @@ from random import randint, choice
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 print(friends[randint(0,4)])
 print(choice(friends))
+
+list_rand = []
+
+for i in range(1, 7):
+    list_rand.append(randint(0, 9))
+
+print(list_rand)
