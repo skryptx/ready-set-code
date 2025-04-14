191c5253f19dd42bf8380c6bf0a5c191eac4b6ec
skryptx <sinnisingla@gmail.com>
2025-04-15 00:31:56 +0530

    chore(java): string

--- java/practice/practice/Main.java
+++ java/practice/practice/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
+        String name1 = "Sinni";
+        String name2 = "Sinni";
+        if(name1.equals(name2)) {
+            System.out.println("equal");
+        }
+        System.out.format("%d, %d", System.identityHashCode(name1), System.identityHashCode(name2));
     }
 }
