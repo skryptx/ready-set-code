9590b733429c9b0f1d98b0f99b00f6a663619132
skryptx <sinnisingla@gmail.com>
2025-05-08 01:15:17 +0530

    feat(js): hoisting

--- js/concepts/hoisting.js
+++ js/concepts/hoisting.js
@@ -1,9 +1,19 @@
+/*
+Explanation on code execution:
+Js engine allocates memory in first run and executes code in second run.
+All variables(initialized with var) are assigned undefined in first run and functions are stored as is.
+
+That means, variables will be undefined if accessed before definition, but functions can be called
+as is without any impact.
+*/
+
 console.log(x);
 // console.log(y); // Will Error out - No Hosting - Temporal Dead zone
 console.log(getName());
 
 var x = 10;
 let y = 20;
+// functions are stored as is, so they can be called anywhere, even before the definition
 function getName() {
   return "Sinni";
 }
