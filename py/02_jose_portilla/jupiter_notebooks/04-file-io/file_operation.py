65deaa13fef371816d15ec03726ee02fe3f329d1
skryptx <sinnisingla@gmail.com>
2025-05-20 21:19:02 +0530

    feat(py): file read

--- py/02_jose_portilla/jupiter_notebooks/04-file-io/file_operation.py
+++ py/02_jose_portilla/jupiter_notebooks/04-file-io/file_operation.py
@@ -1,5 +1,14 @@
-myfile = open('sample.txt')
-while (myfile.closed == False):
+"""
+Read file
+"""
+
+import os
+
+ROOT_DIR = f"{os.path.abspath(os.curdir)}/02_jose_portilla/jupiter_notebooks/04-file-io"
+print(ROOT_DIR)
+
+myfile = open(f"{ROOT_DIR}/sample.txt", mode="r")
+while myfile.closed is False:
     print(myfile.read())
-    if (myfile.seekable()):
+    if myfile.seekable():
         myfile.close()
