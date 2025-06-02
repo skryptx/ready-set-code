d435e16e9eaeb7ccb28dc899f6508ef10814ec78
skryptx <sinnisingla@gmail.com>
2025-06-02 23:55:35 +0530

    feat(py): extensions recommended list

--- py/02_jose_portilla/practice_problems/module_practice/one.py
+++ py/02_jose_portilla/practice_problems/module_practice/one.py
@@ -4,3 +4,5 @@ from functools import reduce
 def add(*args) -> int:
     print(f"__name__ of one.py: {__name__}")
     return reduce(lambda a, b: a+b, list(args), 0)
+
+add() # if executed from this file print will say main in this file
